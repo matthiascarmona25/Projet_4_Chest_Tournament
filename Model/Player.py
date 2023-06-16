f68d52904186fefb6146cce430ce340ce6a38346
CARMONA <matthias97@hotmail.fr>
2023-06-16 16:29:12 +0200

    Création du modèle Player

---
+++ Model/Player.py
@@ -0,0 +1,5 @@
+class Player(object):
+    def __init__(self, lastname, firstname, birth_date):
+        self.lastname = lastname
+        self.firstname = firstname
+        self.birth_date = birth_date
---
+++ main.py
@@ -0,0 +1,5 @@
+from Model.Player import Player
+
+player_01 = Player('DUPOND', 'Jean', '25/10/1997')
+
+print(player_01.firstname)
